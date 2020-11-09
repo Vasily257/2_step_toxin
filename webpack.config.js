16b71706f6d9507dae09d9712ebdca920c506b01
Vasily <vasily.sergeevich.k@gmail.com>
2020-11-09 14:06:17 +0400

    Webpack setting (scss loaders)

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,31 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const { config } = require('process');
+const loader = require('sass-loader');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+
+  return config
+}
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -17,12 +42,16 @@ module.exports = {
   },
   devServer: {
     open: true,
-    port: 4200
+    port: 4200,
+    hot: isDev,
   },
   plugins: [
     new HTMLWebpackPlugin({
       title: 'Toxin',
       template: './index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
     }),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
@@ -32,13 +61,22 @@ module.exports = {
           to: path.resolve(__dirname, 'dist')
         }
       ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
     })
   ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.s[ac]ss$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+
+          },
+        }, 'css-loader',
+          'sass-loader']
       },
       {
         test: /\.(webp|png|jpg|svg|gif)/,
